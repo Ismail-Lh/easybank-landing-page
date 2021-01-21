c7dfdf3205a13400d7037204a54bfc002d4c4294
Ismail-Lh <ismailsaad03@gmail.com>
2021-01-21 02:57:06 +0100

    Animate the hamburger menu

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("hello world");
+const menuIcon = document.querySelector('.header__icon');
+const hamburgerIcon = document.querySelector('#hamburger-icon');
+const closeIcon = document.querySelector('#close-icon');
+
+function toggleMenu (e) {
+   const iconClass = e.target.className;
+   // console.log(iconClass);
+
+   if (iconClass === 'header__icon-hamburger') {
+      closeIcon.style.display = 'inline-block';
+      hamburgerIcon.style.display = 'none';
+   } else if (iconClass === 'header__icon-close') {
+      hamburgerIcon.style.display = 'inline-block';
+      closeIcon.style.display = 'none';
+   }
+}
+
+menuIcon.addEventListener('click', toggleMenu);
+
